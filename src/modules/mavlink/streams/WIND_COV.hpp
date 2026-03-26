acd94dcd686da38f53d4e643fe5cafda9dae2ce1
ChenZehao-rgb <1784359802@qq.com>
2026-03-26 13:53:47 +0800

    add rpm topic  to mavlink

--- src/modules/mavlink/streams/WIND_COV.hpp
+++ src/modules/mavlink/streams/WIND_COV.hpp
@@ -39,6 +39,7 @@
 #include <uORB/topics/thrust_control.h>
 #include <uORB/topics/thrust_data.h>
 #include <uORB/topics/thrust_control_data.h>
+#include <uORB/topics/rpm.h>
 
 class MavlinkStreamWindCov : public MavlinkStream
 {
@@ -64,18 +65,20 @@ private:
 	uORB::Subscription _thrust_control_sub{ORB_ID(thrust_control)};
 	uORB::Subscription _thrust_data_sub{ORB_ID(thrust_data)};
 	uORB::Subscription _thrust_control_data_sub{ORB_ID(thrust_control_data)};
-
+	uORB::Subscription _rpm_sub{ORB_ID(rpm)};
 	bool send() override
 	{
 		thrust_control_s thrustcontrol = {};
 		thrust_control_data_s thrustcontroldata = {};
 		thrust_data_s thrustdata = {};
+		rpm_s rpm = {};
 
 		if (_thrust_data_sub.update(&thrustdata)) {
 			mavlink_wind_cov_t msg{};
 
 			_thrust_control_sub.copy(&thrustcontrol);
 			_thrust_control_data_sub.copy(&thrustcontroldata);
+			_rpm_sub.copy(&rpm);
 			msg.time_usec = thrustdata.timestamp;
 
 			msg.wind_x = thrustdata.thrust_raw_data_1;
@@ -83,7 +86,7 @@ private:
 			msg.wind_z = thrustcontroldata.thrust_error1;
 
 			msg.var_horiz = thrustcontrol.control[0];
-			msg.var_vert = 0.75f;
+			msg.var_vert = rpm.rpm_raw;
 
 			msg.wind_alt = 0.99f;
 
